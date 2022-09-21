3583f92a8ca057e5adcb9ba01216604bdbe41747
Olga <o.panova27@gmail.com>
2022-09-22 00:57:31 +0300

    Add actions in tg-bot, output main menu

--- tg_food_bot/management/commands/_bot.py
+++ tg_food_bot/management/commands/_bot.py
@@ -1,9 +1,15 @@
 from environs import Env
 import logging
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 import redis
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import (
+    Filters,
+    Updater,
+    CommandHandler,
+    CallbackQueryHandler,
+    MessageHandler
+)
 
 
 env = Env()
@@ -39,8 +45,68 @@ def start(update, context):
         chat_id=chat_id,
         document='https://www.africau.edu/images/default/sample.pdf'
     )
-    return 'START'
+    
+    return 'INPUT_USER_NAME'
+    
+    
+def input_user_name(update, context):
+    query = update.callback_query
+    
+    if query.data == 'agree':
+        context.bot.send_message(
+            chat_id=query.message.chat.id,
+            text='Введите ваше имя:',
+        )
+        context.bot.delete_message(
+            chat_id=query.message.chat.id,
+            message_id=query.message.message_id
+        )
+    elif query.data == 'disagree':
+        message_text = 'Очень жаль, что вы не с нами. Если передумаете - введите /start'
+        context.bot.send_message(
+            chat_id=query.message.chat.id,
+            text=message_text,
+        )
+        context.bot.delete_message(
+            chat_id=update.message.chat.id,
+            message_id=update.message.message_id
+        )
+    
+    return 'INPUT_PHONE_NUMBER'
 
+def input_phone_number(update, context):
+    chat_id = update.message.chat_id
+    
+    context.bot.send_message(
+        chat_id=chat_id,
+        text='Введите номер телефона:',
+    )
+    context.bot.delete_message(
+        chat_id=chat_id,
+        message_id=update.message.message_id - 1
+    )
+    
+    return 'MAIN_MENU'
+    
+def main_menu_handler(update, context):
+    chat_id = update.message.chat_id
+    
+    message_text = 'Выберите действие:'
+    inline_keyboard = [
+        [InlineKeyboardButton('Посмотреть рецепты', callback_data='recipes')],
+        [InlineKeyboardButton('Личный кабинет', callback_data='profile')]
+    ]
+    inline_kb_markup = InlineKeyboardMarkup(inline_keyboard)
+    
+    context.bot.send_message(
+        chat_id=chat_id,
+        text=message_text,
+        reply_markup=inline_kb_markup
+    )
+    context.bot.delete_message(
+        chat_id=chat_id,
+        message_id=update.message.message_id - 1
+    )
 
 def handle_users_reply(update, context):
     db = get_database_connection()
@@ -61,6 +127,9 @@ def handle_users_reply(update, context):
 
     states_functions = {
         'START': start,
+        'INPUT_USER_NAME': input_user_name,
+        'INPUT_PHONE_NUMBER': input_phone_number,
+        'MAIN_MENU': main_menu_handler,
     }
 
     print(user_state)
@@ -94,6 +163,7 @@ def main():
     dispatcher = updater.dispatcher
 
     dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(MessageHandler(Filters.text, handle_users_reply))
     dispatcher.add_handler(CommandHandler('start', handle_users_reply))
 
     dispatcher.add_error_handler(error)
