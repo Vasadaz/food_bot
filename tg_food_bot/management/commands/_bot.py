62f5890e3f7a9ff8ecba3f495c634f2d3cd1df3e
HardRope <61661973+HardRope@users.noreply.github.com>
2022-09-21 08:47:01 +0300

    add redis db, add query_handler

--- tg_food_bot/management/commands/_bot.py
+++ tg_food_bot/management/commands/_bot.py
@@ -1,8 +1,9 @@
 from environs import Env
 import logging
 
-from telegram.ext import Updater, CommandHandler
-
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+import redis
 
 
 env = Env()
@@ -12,9 +13,9 @@ logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
-
 logger = logging.getLogger(__name__)
 
+_database = None
 
 def error(state, error):
     logger.warning(f'State {state} caused error {error}')
@@ -22,13 +23,13 @@ def error(state, error):
 
 def start(update, context):
     chat_id = update.message.chat.id
-    
+
     inline_keyboard = [
         [InlineKeyboardButton('Принять', callback_data='agree')],
         [InlineKeyboardButton('Отказаться', callback_data='disagree')]
     ]
     inline_markup = InlineKeyboardMarkup(inline_keyboard)
-    
+
     context.bot.send_message(
         chat_id=chat_id,
         text='Привет! Для начала работы с ботом нужно принять соглашение на обработку персональных данных',
@@ -38,7 +39,53 @@ def start(update, context):
         chat_id=chat_id,
         document='https://www.africau.edu/images/default/sample.pdf'
     )
-    
+    return 'START'
+
+
+def handle_users_reply(update, context):
+    db = get_database_connection()
+
+    if update.message:
+        user_reply = update.message.text
+        chat_id = update.message.chat_id
+    elif update.callback_query:
+        user_reply = update.callback_query.data
+        chat_id = update.callback_query.message.chat_id
+    else:
+        return
+
+    if user_reply == '/start':
+        user_state = 'START'
+    else:
+        user_state = db.get(chat_id)
+
+    states_functions = {
+        'START': start,
+    }
+
+    print(user_state)
+    state_handler = states_functions[user_state]
+    try:
+        next_state = state_handler(update, context)
+        db.set(chat_id, next_state)
+    except Exception as err:
+        error(user_state, err)
+
+
+def get_database_connection():
+    global _database
+    if _database is None:
+        database_password = env.str("DATABASE_PASSWORD")
+        database_host = env.str("DATABASE_HOST")
+        database_port = env.str("DATABASE_PORT")
+        _database = redis.Redis(
+            host=database_host,
+            port=database_port,
+            password=database_password,
+            decode_responses=True,
+        )
+    return _database
+
 
 def main():
     tg_token = env.str('TELEGRAM_TOKEN')
@@ -46,7 +93,8 @@ def main():
     updater = Updater(tg_token, use_context=True)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CallbackQueryHandler(handle_users_reply))
+    dispatcher.add_handler(CommandHandler('start', handle_users_reply))
 
     dispatcher.add_error_handler(error)
 
