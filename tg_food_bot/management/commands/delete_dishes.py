40da49acac2bda0817775d9f25bf3eb6afb1ea13
Nikolai Sysoev <81381305+Vasadaz@users.noreply.github.com>
2022-09-22 17:50:36 +0300

    Add management command upload_dishes

---
+++ tg_food_bot/management/commands/delete_dishes.py
@@ -0,0 +1,38 @@
+import json
+
+from pathlib import Path
+
+from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
+
+from tg_food_bot.models import Dish
+
+
+DISHES_PATH = Path('static/recipes.json')
+
+
+def delete_dish(dish: dict):
+    title = dish['title']
+    try:
+        dish_obj = Dish.objects.get(title=title)
+    except ObjectDoesNotExist:
+        print('Not found dish:', title)
+        return
+    dish_obj.delete()
+    print('Delete dish:', title)
+
+
+def main():
+    with open(DISHES_PATH, 'r', encoding='utf-8') as file:
+        dishes = json.load(file)
+
+    for category, dishes in dishes.items():
+        for dish in dishes:
+            delete_dish(dish)
+
+
+class Command(BaseCommand):
+    help = 'Start delete dishes'
+
+    def handle(self, *args, **options):
+        main()
